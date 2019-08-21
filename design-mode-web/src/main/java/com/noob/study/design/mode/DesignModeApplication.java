08e961dabe4bc8028e901624a6aed108f8e1cd85
liuym <2417788343@qq.com>
2019-08-21 16:39:29 +0800

    first commit

---
+++ design-mode-web/src/main/java/com/noob/study/design/mode/DesignModeApplication.java
@@ -0,0 +1,17 @@
+package com.noob.study.design.mode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: liuym
+ * @Date: 2019/8/21 15:40
+ * @Description：
+ */
+@SpringBootApplication
+public class DesignModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModeApplication.class, args);
+    }
+}
---
+++ design-mode-web/src/main/java/com/noob/study/design/mode/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.noob.study.design.mode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: liuym
+ * @Date: 2019/8/21 15:53
+ * @Description：
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
