36d6225f1300936c59e06c3420fc3555db6681f9
liuym <2417788343@qq.com>
2019-08-21 17:40:59 +0800

    设计原则-开闭原则

---
+++ design-mode-web/src/main/java/com/noob/study/design/mode/principle/openclose/DiscountServiceImpl.java
@@ -0,0 +1,30 @@
+package com.noob.study.design.mode.principle.openclose;
+
+/**
+ * @Auther: liuym
+ * @Date: 2019/8/21 17:35
+ * @Description：
+ */
+public class DiscountServiceImpl extends CourseServiceImpl {
+
+    public DiscountServiceImpl(Integer id, String name, Double price) {
+        super(id, name, price);
+    }
+
+    /**
+     * 原价
+     * @return
+     */
+    public Double originalPrice(){
+        return super.price();
+    }
+
+    /**
+     * 折扣价
+     * @return
+     */
+    @Override
+    public Double price() {
+        return super.price()*0.8;
+    }
+}
