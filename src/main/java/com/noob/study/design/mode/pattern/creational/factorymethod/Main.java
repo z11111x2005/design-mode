f0a267ded609ccfc8d3a87e2895beb1c4a6cfc3a
z11111x2005 <2417788343@qq.com>
2019-09-05 01:10:19 +0800

    修改注释

--- src/main/java/com/noob/study/design/mode/pattern/creational/factorymethod/Main.java
+++ src/main/java/com/noob/study/design/mode/pattern/creational/factorymethod/Main.java
@@ -3,7 +3,7 @@ package com.noob.study.design.mode.pattern.creational.factorymethod;
 /**
  * @Auther: liuym
  * @Date: 2019/9/4 14:29
- * @Description：简单工厂模型
+ * @Description：工厂方法模型
  */
 public class Main {
     public static void main(String[] args) {
